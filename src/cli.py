2b97e1029f50ecc21590a35efc482b8c23ef9290
Vicente-G <vicentegonsal2000@gmail.com>
2023-09-07 01:06:09 -0300

    feat: added template addition to config

--- src/cli.py
+++ src/cli.py
@@ -1,9 +1,11 @@
 from git import git_add, git_branch, git_commit, git_download, git_upload
+from config import add_template
 from github import gh_make_pr
 from template import gh_init
 from argparse import RawTextHelpFormatter, ArgumentParser
 
 FUNCTIONS = {
+    "new": add_template,
     "init": gh_init,
     "add": git_add,
     "commit": git_commit,
@@ -15,13 +17,14 @@ FUNCTIONS = {
 
 description = """A CLI with Vicente-G's day-to-day functions
 
-init:     \tcreate a new private GitHub repository from a template
-pr:       \tcreate a new pull request with a guided interface
-add:      \tadd any files to this repo or all of them with a dot
-branch:   \tcreate and/or switch to a branch in this repo
-commit:   \tmake a commit with a guided interface into this repo
-download: \tclone a repo with a given URL or pull from origin
-upload:   \tset origin to a given URL or push from this repo
+new       \tconfigurate a new template for the init command
+init      \tcreate a new private GitHub repository from a template
+pr        \tcreate a new pull request with a guided interface
+add       \tadd any files to this repo or all of them with a dot
+branch    \tcreate and/or switch to a branch in this repo
+commit    \tmake a commit with a guided interface into this repo
+download  \tclone a repo with a given URL or pull from origin
+upload    \tset origin to a given URL or push from this repo
 """
 
 parser = ArgumentParser(
@@ -40,4 +43,10 @@ parser.add_argument(
 parser.add_argument("-v", "--version", action="version")
 parser.add_argument("-y", action="store_true", help="skip all confirmation prompts")
 args, unknown = parser.parse_known_args()
-FUNCTIONS[args.command](*unknown, skip = args.y)
+
+try:
+    status = FUNCTIONS[args.command](*unknown, skip = args.y)
+    exit(status)
+except KeyboardInterrupt:
+    print("\nvg: error: operation cancelled by user")
+    exit(1)
--- src/config.py
+++ src/config.py
@@ -1,7 +1,44 @@
 import json
+import os
+
+CONFIG_FILE = "/usr/local/lib/vg/config.json"
 
 def load_config():
     config = {}
-    with open("/usr/local/lib/vg/config.json", "r") as file:
+    with open(CONFIG_FILE, "r") as file:
         config = json.load(file)
     return config
+
+def save_config(config):
+    with open(CONFIG_FILE, "w") as file:
+        json.dump(config, file, indent=4)
+
+def add_template(skip = False):
+    config = load_config()
+    user, temps = config["username"], config["templates"]
+    name = input("Template name: (no spaces) ").lower().split(" ")[0]
+    keys = list(filter(lambda c: c not in temps, [char for char in name]))
+    if len(keys) == 0:
+        print("vg: error: invalid template name, please try again")
+        return 1
+    display = "(" + "/".join(keys) + ")"
+    while (key := input(f"Choose a key: {display} ")) not in keys:
+        print("Choose a key within the brackets")
+    name = name[:name.find(key)] + key.upper() + name[name.find(key)+1:]
+    cfile = input("Config file: (package.json) ") or "package.json"
+    owner = input(f"Template owner: ({user}) ") or user
+    repo = input("Template repository name: ")
+    url = f"http://github.com/{owner}/{repo}/blob/main/{cfile}"
+    flags = "--head --silent --fail"
+    check = os.system(f"curl {flags} {url} &> /dev/null")
+    if check != 0:
+        print("vg: error: repo doesn't respond, please try again")
+        return 1
+    config["templates"][key] = {
+        "display": name.replace(key.upper(), f"[{key.upper()}]"),
+        "config-file": cfile,
+        "url": f"git@github.com:{owner}/{repo}.git"
+    }
+    save_config(config)
+    print(f"Template {name} added successfully!")
+    return 0
